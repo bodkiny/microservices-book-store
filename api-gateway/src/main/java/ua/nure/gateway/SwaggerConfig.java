cfe8e2ca50aa25277e1b25038883496a0749e31a
Bohdan Rudenko <brudenko2804@gmail.com>
2025-05-17 20:35:12 +0300

    Fix Swagger config and update service routing identifiers
    Adjusted regex in SwaggerConfig for proper service name extraction and corrected RewritePath filter syntax. Updated service ID in application.yml to ensure consistency with routing rules.

--- api-gateway/src/main/java/ua/nure/gateway/SwaggerConfig.java
+++ api-gateway/src/main/java/ua/nure/gateway/SwaggerConfig.java
@@ -30,7 +30,7 @@ class SwaggerConfig {
         definitions.stream()
                 .filter(routeDefinition -> routeDefinition.getId().matches(".*-service"))
                 .forEach(routeDefinition -> {
-                    String name = routeDefinition.getId().replaceAll(".*-service", "");
+                    String name = routeDefinition.getId().replaceAll("-service", "");
                     AbstractSwaggerUiConfigProperties.SwaggerUrl swaggerUrl =
                             new AbstractSwaggerUiConfigProperties.SwaggerUrl(
                                     name, DEFAULT_API_DOCS_URL + "/" + name, null);
